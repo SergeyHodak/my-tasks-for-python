ec86b68f6205168decc99b74cd25e494f0842d28
SergeyHodak <87858809+SergeyHodak@users.noreply.github.com>
2021-08-05 19:11:40 +0300

    Update tasks-8.py

--- tasks-8.py
+++ tasks-8.py
@@ -3,23 +3,9 @@ def is_all_upper(text: str) -> bool:
     Проверьте, все ли символы в данной строке написаны заглавными буквами.
     Если строка пуста или в ней нет букв - функция должна вернуть True. 
     """
-    b = True  #правда
-    d = False #лож
-    FullProbel = text[1:-1].isspace()  # True, если строка содержит только пробельные символы
-    FullBukva = text[1:-1].isalpha()   # True, если все символы в строке являются буквами
-    FullBukvaUp = text[1:-1].isupper() # True, если все символы в данной строке в верхнем регистре
-    if FullBukva == b:
-        return b
-    elif FullBukvaUp == b:
-        return b
-    elif len(text) == 0:
-        return b
-    elif FullProbel == b:
-        return b
-    elif text[0] in '0123456789/':  # есди первый символ с text есть в списке '0123456789/'
-        return b
-    else:
-        return d
+    return list(map(lambda x: x.upper(), text)) == list(text)
+    # если (возвести в верхний регистр "text" в виде листа) и сравнить с
+    # (преобразованной строкой в лист) получим на выходе логику правда или фальш
 
 
 if __name__ == '__main__':
