f23de504d3ae44019732af27d852e6ef079f2c76
SergeyHodak <87858809+SergeyHodak@users.noreply.github.com>
2021-08-07 13:07:44 +0300

    Update tasks-0020.py

--- tasks-0020.py
+++ tasks-0020.py
@@ -6,21 +6,20 @@ def left_join(phrases: tuple) -> str:
         слова "right" на слова "left", даже если это часть другого слова.
         Все строки даны в нижнем регистре.
     """
-    b = str()
-    for i in range(len(phrases)):  # цикл от индекса ноль до конечного индекса кортежа (phrases)
-        a = str(phrases[i])
-        bn = a.replace("right", "left")
-        b = b + bn + ","
-    b = (b[0:-1])
-    return b    
+    b = str()  # пустая строка для формировки результата на вывод
+    for i in range(len(phrases)):  # пробежка по кортежу (phrases)
+        bn = phrases[i].replace("right", "left")  # заменяем указанную фразу другой указанной фразой 
+        b += bn + ","  # записываем проверенное слово и запятую добавляем
+    return b[:-1]  # выдаем результат изавившись от крайней запятой
+    
 
 if __name__ == '__main__':
-    left_join('Example:')
+    left_join('Пример:')
     print(left_join(("left", "right", "left", "stop")))
     
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # Эти "asserts" используются только для самопроверки и не требуются для автоматического тестирования.
     assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop", "All to left"
     assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
     assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
     assert left_join(("enough", "jokes")) == "enough,jokes", "Nothing to replace"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+    print("Кодирование завершено? Нажмите 'Check', чтобы просмотреть свои тесты и получить отличные награды!")
