e5323f7f13b89e40674720f8b8986eb9b6e91108
SergeyHodak <87858809+SergeyHodak@users.noreply.github.com>
2021-08-10 19:50:32 +0300

    Update tasks-0022.py

--- tasks-0022.py
+++ tasks-0022.py
@@ -22,11 +22,11 @@ def days_diff(a, b):
 
 
 if __name__ == '__main__':
-    print("Example:")
+    print("Пример:")
     print(days_diff([1, 1, 1], [9999, 12, 31]))  # 3652058
 
-    # These "asserts" are used for self-checking and not for an auto-testing
+    # Эти "asserts" используются для самопроверки, а не для автоматического тестирования.
     assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
     assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
     assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+    print("Кодирование завершено? Нажмите 'Check', чтобы получить отличные награды!")
