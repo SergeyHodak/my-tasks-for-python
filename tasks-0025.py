2710e285405d2c35aa876dea6eae928d062169ad
SergeyHodak <87858809+SergeyHodak@users.noreply.github.com>
2021-08-11 16:15:57 +0300

    Update tasks-0025.py

--- tasks-0025.py
+++ tasks-0025.py
@@ -1,6 +1,6 @@
 def bigger_price(limit: int, data: list) -> list:
     """
-    ТОП самых дорогих товаров 
+    ТОП самых дорогих товаров.
     Дана таблица всех доступных продуктов на складе.
     Данные представлены в виде списка словарей (a list of dicts)
     Ваша миссия тут - это найти ТОП самых дорогих товаров.
@@ -8,47 +8,36 @@ def bigger_price(limit: int, data: list) -> list:
     а сами данные по товарам будут переданы вторым аргументом.
     тип данных = словарь - dict
     """
-    a = []  # пустой список для процессов сортировки
-    for i in range(0, len(data)):  # пробежка от нуля к концу списка словарей
-        a.append(data[i]["price"])  # добавить в конец значение цены с положения и
-    a.sort(reverse=True)  # сортирует на убывание
-    b = []  # пустой список для вывода результата
-    for i in range(0, len(a)):  # пробежка от нуля к концу отсортированного списка значений
-        s = 0  # флаг для вайла
-        f = 0  # шагатель
-        while s == 0:  # пока нет тормоза "s>0"
-            if data[f]["price"] == a[i]:  # если строка "f" из списка словарей "data" своей ценой равна с позицией "i" из отсортированного списка цен "a"
-                b.append(data[f])  # вносим ее в список "b" для вывода
-                s = 1  # прекращаем цикл вайл
-            else:  # если строка "f" из списка словарей "data" своей ценой НЕ равна с позицией "i" из отсортированного списка цен "a"
-                f += 1  # повышаем шаг
-    return b[0:limit]  # отдать результат согласно первого полученного аргумента функции, от до лимита
+    a = []  # список для сортировки цен
+    for i in range(0, len(data)):  # пробежка по ценникам
+        a.append(data[i]["price"])  # записать цену
+    a[::-1] = sorted(a)  # отсортитровать и поставить на убывание
+    b = []  # список для выдачи результата
+    for i in range(0, limit):  # пробежать limit раз
+        for j in range(0, len(data)):  # пробежка по ценникам
+            if str(a[i]) in str(data[j]):  # если цена a[i] есть в позиции data[j]
+                b.append(data[j])  # вносим ее в список "b" для вывода
+    return b  # выдать результат
 
 
 if __name__ == '__main__':
     from pprint import pprint
-    print('Example:')
-    pprint(bigger_price(2, [
-        {"name": "bread", "price": 100},
-        {"name": "wine", "price": 138},
-        {"name": "meat", "price": 15},
-        {"name": "water", "price": 1}
-    ]))
+    print('Пример:')
+    pprint(bigger_price(2, [{"name": "bread", "price": 100},
+                            {"name": "wine", "price": 138},
+                            {"name": "meat", "price": 15},
+                            {"name": "water", "price": 1}]))
 
-    # These "asserts" using for self-checking and not for auto-testing
-    assert bigger_price(2, [
-        {"name": "bread", "price": 100},
+    # Эти "asserts" используются для самопроверки, а не для автоматического тестирования.
+    assert bigger_price(2, [{"name": "bread", "price": 100},
+                            {"name": "wine", "price": 138},
+                            {"name": "meat", "price": 15},
+                            {"name": "water", "price": 1}]) == [
         {"name": "wine", "price": 138},
-        {"name": "meat", "price": 15},
-        {"name": "water", "price": 1}
-    ]) == [
-        {"name": "wine", "price": 138},
-        {"name": "bread", "price": 100}
-    ], "First"
+        {"name": "bread", "price": 100}], "First"
 
-    assert bigger_price(1, [
-        {"name": "pen", "price": 5},
-        {"name": "whiteboard", "price": 170}
-    ]) == [{"name": "whiteboard", "price": 170}], "Second"
+    assert bigger_price(1, [{"name": "pen", "price": 5},
+                            {"name": "whiteboard", "price": 170}]) == [
+        {"name": "whiteboard", "price": 170}], "Second"
 
-    print('Done! Looks like it is fine. Go and check it')
+    print('Готово! Похоже, все в порядке. Иди и проверь это')
