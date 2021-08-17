b7a8673d6036af51394a8ebdda605b9624290d25
SergeyHodak <4Kserge4Kserge4K@gmail.com>
2021-08-17 11:54:33 +0300

    tasks-0051

--- tasks-0051.py
+++ tasks-0051.py
@@ -1,25 +1,28 @@
 """
-Ваша задача - написать функцию, которая преобразовывает текст (название другой функции) из формата CamelCase, принятого в JavaScript (MyFunctionName) в формат, принятый в Python (my_function_name), где все буквы - маленькие, а слова соединены знаком нижнего подчеркивания "_".
-
-Входные данные: Название функции как строка в CamelCase
-
-Output: То же самое название, но в under_score
-
-Предусловия :
-0 < len(string) <= 100
-Во входящих данных не будет чисел или пустых строк
+    Ваша задача - написать функцию, которая преобразовывает текст (название другой функции) из формата CamelCase,
+    принятого в JavaScript (MyFunctionName) в формат, принятый в Python (my_function_name), где все буквы -
+    маленькие, а слова соединены знаком нижнего подчеркивания "_".
 """
 def from_camel_case(name: str) -> str:
-    #replace this for solution
-    return name
+    a = str()  # пустая строка
+    for i in name:  # пробежка по имени в формате "CamelCase" для JavaScript
+        if i != name[0]:  # если это не перввая позиция
+            if i.isupper():  # если в верхнем регистре
+                a = str(a) + str("_") + str(i.lower())  # записать в нижнем регистре и перед нижнее подчеркивание
+            else:  # в нижнем регистре
+                a = str(a) + str(i)  # записать как есть
+        else:  # первая позиция
+            a = str(a) + str(name[0].lower())  # записать в нижнем регистре
+    return a  # выдать в формате для Python
+
 
 if __name__ == '__main__':
-    print("Example:")
+    print("Пример:")
     print(from_camel_case("Name"))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # Эти "asserts" используются только для самопроверки и не требуются для автоматического тестирования.
     assert from_camel_case("MyFunctionName") == "my_function_name"
     assert from_camel_case("IPhone") == "i_phone"
     assert from_camel_case("ThisFunctionIsEmpty") == "this_function_is_empty"
     assert from_camel_case("Name") == "name"
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+    print("Кодирование завершено? Нажмите 'Check', чтобы получить отличные награды!")
